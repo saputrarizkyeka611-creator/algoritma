6ba0209974cc9293dda122c4cdcb8830857a1f63
saputrarizkyeka611-creator <saputrarizkyeka611@gmail.com>
2026-09-09 10:32:10 +0700

    Add files via upload

---
+++ bab3.py
@@ -0,0 +1,19 @@
+ama   = "rizky " 
+umur  = 16
+tinggi = "166 cm"
+lulus  = True
+print("nama:", nama)
+print("umur:", umur)
+print("tinggi:", tinggi)
+print("lulus:", lulus)
+print()
+produk="sepatu bola"
+stok=15
+harga=250000
+tersedia="true"
+print("nama poduk : ",produk)
+print("Stok :",harga)
+print("harga ($);",harga)
+print("Tersedia :",Tersedia)
+
+
