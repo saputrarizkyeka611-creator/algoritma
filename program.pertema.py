1ff6e4546e56b0d614aecd86d7c3010b992dcd4a
saputrarizkyeka611-creator <saputrarizkyeka611@gmail.com>
2026-09-09 10:33:24 +0700

    Add files via upload

---
+++ program.pertema.py
@@ -0,0 +1,21 @@
+
+
+rint("===============================")
+print        ("SELAMAT DATANG DI PYTHON")
+print("===============================")
+print()
+print("PROGRAM INI DI BUAT OLEH:RIZKY EKA SAPUTRA")
+print("SEKOLAH SMK TJP TUBAN")
+print("TAHUN     :",2026)
+print()
+print(10    +      5)
+print(20    /      4)
+print(15    //     4)
+print(4+7)
+print(2**8)
+a=27
+b=4
+print(a=b)
+print(a-b)
+print()
+      
